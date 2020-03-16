32258302a07ec9adbbe7fef97741d16885b2868c
ailequal <37016865+ailequal@users.noreply.github.com>
2020-03-16 17:14:03 +0100

    Working on controller

--- app/Http/Controllers/CdsController.php
+++ app/Http/Controllers/CdsController.php
@@ -7,6 +7,22 @@ use Illuminate\Http\Request;
 class CdsController extends Controller
 {
 		public function index() {
-			return view('cds');
+			$data = [
+				'cds' => [
+					[
+						'cover' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+						'album' => 'New Jersey',
+						'artist' => 'Bon Jovi',
+						'year' => '1988'
+					],
+					[
+						'cover' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+						'album' => 'Live at Wembley 86',
+						'artist' => 'Queen',
+						'year' => '1992'
+					]
+				]
+			];
+			return view('cds', $data);
 		}
 }
